d5704d2b0b4a8ef46b528f136419555863a5c8a4
kranthi kumar <g.kranthi2507@gmail.com>
2024-01-09 22:39:11 +0530

    useEffect and useCallBack Hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { useState, useEffect, useCallback } from "react";
 import { BiCalendar } from "react-icons/bi";
 import Search from "./components/Search";
 import AddAppointment from "./components/AddAppointment";
-import appointmentList from "./data.json";
 import AppointmentInfo from "./components/AppointmentInfo";
 
 function App() {
+  let [appointmentList, setAppointmentList] = useState([]);
+  const fetchData = useCallback(() => {
+    fetch('./data.json')
+      .then(response => response.json())
+      .then(data => {
+        setAppointmentList(data)
+      });
+  }, [])
+  useEffect(() => {
+    fetchData()
+  }, [fetchData])
+  console.log(appointmentList);
   return (
     <div className="App contaier mx-auto mt-3 font-thin">
       <h1 className="text-5xl mb-3"><BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
@@ -12,7 +24,7 @@ function App() {
       <Search />
       <ul className="divide-y divide-gray-200">
         {appointmentList.map(appointment => (
-          <AppointmentInfo key={appointment.id} appointment={appointment}/>
+          <AppointmentInfo key={appointment.id} appointment={appointment} />
         ))}
 
       </ul>
