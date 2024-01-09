eb4143423c757c91d2501b401c800411ac661bbf
kranthi Kumar <kranthi@lightsapien.com>
2024-01-09 16:08:52 +0530

    create components

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { BiCalendar } from "react-icons/bi";
+import Search from "./components/Search";
+import AddAppointment from "./components/AddAppointment";
+import appointmentList from "./data.json";
+import AppointmentInfo from "./components/AppointmentInfo";
+
+function App() {
+  return (
+    <div className="App contaier mx-auto mt-3 font-thin">
+      <h1 className="text-5xl mb-3"><BiCalendar className="inline-block text-red-400 align-top" />Your Appointments</h1>
+      <AddAppointment />
+      <Search />
+      <ul className="divide-y divide-gray-200">
+        {appointmentList.map(appointment => (
+          <AppointmentInfo key={appointment.id} appointment={appointment}/>
+        ))}
+
+      </ul>
+    </div>
+  );
+}
+
+export default App;
